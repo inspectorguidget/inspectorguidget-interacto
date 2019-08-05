308283e53a34f3b7dc332e4c49c184d0bb616f9b
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-08-05 09:33:32 +0200

    update(commandExtractor) building commandExtractor

--- src/main/java/fr/inria/inspectorguidget/CommandExtractor.java
+++ src/main/java/fr/inria/inspectorguidget/CommandExtractor.java
@@ -2,15 +2,18 @@ package fr.inria.inspectorguidget;
 
 
 import spoon.reflect.code.CtConstructorCall;
-import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.code.CtLambda;
+import spoon.reflect.code.CtLocalVariable;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.path.CtRole;
 import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.filter.AbstractFilter;
+import spoon.support.reflect.code.CtVariableReadImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
@@ -28,51 +31,83 @@ public class CommandExtractor {
 
     public void extractCommand(CtInvocation invocation){
 
-        CtInvocation invoc = invocation.getElements(new AbstractFilter<CtInvocation>() {
-            @Override
-            public boolean matches(final CtInvocation invo) {
-                System.out.println(invo.getShortRepresentation());
-                return true;
-            }}).get(0);
+        // get access to invocation nodeBinder()
+        List<CtElement> children = invocation.getDirectChildren();
+        CtInvocation invoc = null;
 
-        // afficher les arguments de invoc
-        /*List<CtTypeReference> args = invocation.getActualTypeArguments();
-        for(CtTypeReference arg : args){
-            System.out.println(arg);
-        }*/
-        System.out.println("---------------");
+        while(children.get(0) instanceof CtInvocation){
+            invoc = (CtInvocation) children.get(0);
+            children = invoc.getDirectChildren();
+        }
 
-        /* pb ici: on peut cacher la fonction dans une variable et faire simplement un CtVariableRead : nodeBinder(new Press(), creation) */
-       /* List<CtConstructorCall> commands;
+        // get Arguments of binder:
+        List<CtElement> args = new ArrayList<>();
+        for (CtElement child : children){
+            if(child.getRoleInParent().equals(CtRole.ARGUMENT)){
+                args.add(child);
+            }
+        }
+
+        //get supplier of command (2 arg in nodeBinder, first in others)
+        CtElement supplier = args.size()==2? args.get(1): args.get(0);
+
+        // CtLabmda or CtFieldRead (variable)
+
+        if(supplier instanceof CtLambda)
+            extractCommandLambda((CtLambda) supplier);
+        else if (supplier instanceof CtVariableReadImpl)
+            extractCommandVariable((CtVariableReadImpl) supplier);
+        else
+            logr.log(Level.WARNING, "not able to identify command");
+
+        System.out.println("-------------");
+
+    }
+
+    public void extractCommandLambda(CtLambda lambda){
+        List<CtConstructorCall> commands;
         try {
 
-            commands = invocation.getElements(new AbstractFilter<CtConstructorCall>() {
+            commands = lambda.getElements(new AbstractFilter<CtConstructorCall>() {
                 @Override
                 public boolean matches(final CtConstructorCall constructorCall) {
 
                     Set<CtTypeReference<?>> typeReferences = constructorCall.getExecutable().getReferencedTypes();
-
                     for(CtTypeReference<?> typeRef :typeReferences){
-
-                        boolean isIn = isInCommand(typeRef.getSimpleName());
-                        System.out.println(typeRef.getSimpleName() + " : " + isIn);
+                        if(isInCommand(typeRef.getSimpleName()))
+                            return true;
                     }
+                    System.out.println("pas trouve la COMMANDE");
                     return false;
                 }
             });
 
-            for(CtConstructorCall command : commands) {
-                System.out.println(command);
-            }
+            CtConstructorCall command = commands.get(0); // command to return
+           System.out.println(command);
 
         } catch (Exception e){
             logr.log(Level.WARNING, "Problem identifying command of widget" );
         }
-*/
-        //récupère classe
-        // 1 - on l'affiche
-        // 2 - on créer commande et on la renvoie?
-        //System.out.println(invocation.toString());
+    }
+
+    public void extractCommandVariable(CtVariableReadImpl variable){
+        CtMethod method = variable.getParent(CtMethod.class);
+        CtLambda lambda;
+        try {
+            lambda = method.getElements(new AbstractFilter<CtLambda>() {
+                @Override
+                public boolean matches(final CtLambda elt) {
+                    CtLocalVariable varDef = elt.getParent(CtLocalVariable.class);
+                    if(varDef == null)
+                        return false;
+
+                    return varDef.getSimpleName().compareTo(variable.toString()) == 0;
+                }
+            }).get(0);
+            extractCommandLambda(lambda);
+        } catch (Exception e){
+            logr.log(Level.WARNING,"Impossible to identify command");
+        }
     }
 
     private boolean isInCommand(String className){
