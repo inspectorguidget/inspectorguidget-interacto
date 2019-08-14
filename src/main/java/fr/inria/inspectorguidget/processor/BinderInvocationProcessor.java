e5403d025229a51b410ab97075edfabe4458b30c
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-08-14 09:38:40 +0200

    new(license) adding license file and headers

--- src/main/java/fr/inria/inspectorguidget/processor/BinderInvocationProcessor.java
+++ src/main/java/fr/inria/inspectorguidget/processor/BinderInvocationProcessor.java
@@ -1,3 +1,17 @@
+/*
+ * This file is part of InspectorGuidget.
+ * InspectorGuidget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * InspectorGuidget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with InspectorGuidget.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package fr.inria.inspectorguidget.processor;
 
 import org.jetbrains.annotations.NotNull;
@@ -5,13 +19,11 @@ import org.jetbrains.annotations.NotNull;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.reference.CtExecutableReference;
 import spoon.reflect.visitor.filter.AbstractFilter;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class BinderInvocationProcessor extends AbstractProcessor<CtInvocation> {
